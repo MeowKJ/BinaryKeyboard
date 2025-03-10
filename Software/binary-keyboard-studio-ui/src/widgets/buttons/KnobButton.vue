ee8cd9d9e7e97cea076e4b3776d00ea7b6b3c19e
KJ <ijink@icloud.com>
2025-03-10 16:46:57 +0800

    feat✨: 优化旋钮按钮组件，增加表示方向的图标

--- Software/binary-keyboard-studio-ui/src/widgets/buttons/KnobButton.vue
+++ Software/binary-keyboard-studio-ui/src/widgets/buttons/KnobButton.vue
@@ -1,8 +1,16 @@
 <template>
-    <div :class="['knob-button', { 'active-button': index.includes(currentIndex) }]">
-        <Button v-for="(segment, i) in 3" :key="i" :label="label[i]"
+    <div class="knob-button">
+        <Button v-for="(segment, i) in 3" :key="i"
             :class="['knob-segment', `segment-${i}`, { 'active-button-segment': index[i] === currentIndex }]"
-            severity="secondary" @click="handleClick(index[i])" />
+            severity="secondary" @click="handleClick(index[i])">
+            <div class="button-content">
+
+                <i v-if="i === 0" class="pi pi-replay icon"></i> <!-- 第一个按钮：pi-refresh -->
+
+                <i v-if="i === 2" class="pi  pi-refresh icon"></i> <!-- 第三个按钮：pi-replay -->
+                <span>{{ label[i] }}</span>
+            </div>
+        </Button>
     </div>
 </template>
 
@@ -19,7 +27,7 @@ const emit = defineEmits(["update:index", "open-dialog"]);
 
 const handleClick = (i: number) => {
     emit("update:index", i);
-    emit("open-dialog", i); // 触发打开 dialog 事件
+    emit("open-dialog", i);
 };
 </script>
 
@@ -40,29 +48,42 @@ const handleClick = (i: number) => {
     font-size: 1.2em;
     border: none;
     cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 }
 
-.knob-button:hover {
-    border: 2px solid var(--p-button-outlined-primary-border-color);
-}
-
-.knob-segment:hover {
-    border: none !important;
-    outline: none !important;
-    box-shadow: none !important;
-}
-
-.active-button {
-    border: 2px solid var(--p-button-outlined-primary-border-color);
-}
-
+/* 激活的按钮 */
 .active-button-segment {
     background-color: var(--p-button-outlined-primary-border-color);
     color: var(--p-button-outlined-primary-text-color);
 }
 
-.active-button-segment:hover {
-    background-color: var(--p-button-outlined-primary-border-color) !important;
-    color: var(--p-button-outlined-primary-text-color);
+/* 图标和文字垂直排列 */
+.button-content {
+    display: flex;
+    flex-direction: column;
+    /* 垂直排列 */
+    align-items: center;
+    gap: 4px;
+    /* 图标和文字之间的间距 */
+}
+
+/* 图标样式优化 */
+.icon {
+    font-size: 1.5em;
+}
+
+.segment-1 {
+    border-radius: 0;
+    border-left: 3px solid var(--p-content-background) !important;
+    border-right: 3px solid var(--p-content-background) !important;
+    border-top: none !important;
+    border-bottom: none !important;
+}
+
+.segment-0,
+.segment-2 {
+    border: none !important;
 }
 </style>
