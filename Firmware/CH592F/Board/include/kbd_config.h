aa5ea36967db50930d5e16eb63e0312a4b555e0c
KJ <ijink@qq.com>
2026-01-07 17:18:15 +0800

    feat: 增加 WS2812B RGB 驱动代码  - 基于 TMR1 PWM + DMA 实现  Closes #11

--- Firmware/CH592F/Board/include/kbd_config.h
+++ Firmware/CH592F/Board/include/kbd_config.h
@@ -5,32 +5,43 @@
 
 #define KBD_LAYOUT_5KEY
 
-typedef enum {
+typedef enum
+{
     GPIO_PORT_A = 0,
     GPIO_PORT_B = 1,
 } gpio_port_t;
 
-typedef struct {
+typedef struct
+{
     gpio_port_t port;
-    uint32_t pin;   // 位掩码：GPIO_Pin_x
+    uint32_t pin; // 位掩码：GPIO_Pin_x
 } kbd_key_pin_t;
 
-/* ======================= 功能键（独立）======================= */
+/* ======================= 特殊IO ======================= */
+
+// WS2812: TMR1
+#define WS2812_PORT GPIO_PORT_A
+#define WS2812_PIN GPIO_Pin_10
+
+// WS2812 ENABLE: PA9
+#define WS2812_EN_PORT GPIO_PORT_A
+#define WS2812_EN_PIN GPIO_Pin_9
+/* ======================= 功能键======================= */
 /* 只需要下降沿触发（按下事件），不需要释放事件 */
 
 #define KBD_FN_NUM_KEYS 2
 
 // BOOT: PB22
 #define KBD_FN_BOOT_PORT GPIO_PORT_B
-#define KBD_FN_BOOT_PIN  GPIO_Pin_22
+#define KBD_FN_BOOT_PIN GPIO_Pin_22
 
 // FN1: PB14
-#define KBD_FN1_PORT     GPIO_PORT_B
-#define KBD_FN1_PIN      GPIO_Pin_14
+#define KBD_FN1_PORT GPIO_PORT_B
+#define KBD_FN1_PIN GPIO_Pin_14
 
 // FN2: PB15
-#define KBD_FN2_PORT     GPIO_PORT_B
-#define KBD_FN2_PIN      GPIO_Pin_15
+#define KBD_FN2_PORT GPIO_PORT_B
+#define KBD_FN2_PIN GPIO_Pin_15
 
 /* ======================= 普通键（键盘键位）======================= */
 #ifdef KBD_LAYOUT_5KEY
@@ -38,19 +49,19 @@ typedef struct {
 
 // 五键映射
 #define KBD_K1_PORT GPIO_PORT_B
-#define KBD_K1_PIN  GPIO_Pin_4
+#define KBD_K1_PIN GPIO_Pin_4
 
 #define KBD_K2_PORT GPIO_PORT_B
-#define KBD_K2_PIN  GPIO_Pin_7
+#define KBD_K2_PIN GPIO_Pin_7
 
 #define KBD_K3_PORT GPIO_PORT_B
-#define KBD_K3_PIN  GPIO_Pin_12
+#define KBD_K3_PIN GPIO_Pin_12
 
 #define KBD_K4_PORT GPIO_PORT_B
-#define KBD_K4_PIN  GPIO_Pin_13
+#define KBD_K4_PIN GPIO_Pin_13
 
 #define KBD_K5_PORT GPIO_PORT_A
-#define KBD_K5_PIN  GPIO_Pin_13
+#define KBD_K5_PIN GPIO_Pin_13
 
 #endif /* KBD_LAYOUT_5KEY */
 
